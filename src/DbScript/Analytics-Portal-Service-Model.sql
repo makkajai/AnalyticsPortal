ee664615e47a6d4793d4fc4dffc47b93ba3048f6
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2013-04-23 23:41:16 +0530

    Initial commit for the project
    Adding solution file, project files, and a db script for initializing
    the database.

---
+++ src/DbScript/Analytics-Portal-Service-Model.sql
@@ -0,0 +1,40 @@
+﻿/* use a separate schema */
+/*create views for tables that might need to be independent eventually - note that these are read-only as views, and this is by design
+so that the analytics database is not supposed to manipulate any data in these tables*/
+
+Create schema analytics;
+
+Create view analytics.students as 
+select * from students;
+
+create or replace view analytics.activities as 
+select board_id as activity_id, name, difficulty, title, description, prerequisite, goal from boards;
+
+
+/* these tables are specific to analytics database, so it is fine to use them without having views*/
+
+create table analytics.subjects
+(
+  subject_id SERIAL,
+  subject_name TEXT
+);
+
+
+create table analytics.topics
+(
+  topic_id SERIAL,
+  topic_name TEXT
+);
+
+create table analytics.subtopics
+(
+  subtopic_id SERIAL,
+  topic_id INT,
+  subtopic_name TEXT
+);
+
+create table analytics.activitiestosubtopics
+(
+  subtopic_id INT,
+  activity_id INT
+);
