1531d7512fd3b51b635035f376d25b5ec85c9481
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2013-04-28 21:55:27 +0530

    Intermediate commit
    Committing whatever is done at least locally - will need to fix the
    summary page javascript.

---
+++ src/AnalyticsPortal/AnalyticsPortal.Web/Scripts/summary/summary.js
@@ -0,0 +1,21 @@
+﻿function SummaryViewModel() {
+    this.studentLogin = ko.observable(""); //this is what is used for fetching data - should be updated
+
+    this.studentSummary = ko.observableArray([{Duration: "100"}, {Duration: "200"}]);
+
+    this.getData = function() {
+        //make a call to the function
+        //get data
+        var summaryUrl = "/summary/" + this.studentLogin();
+        jQuery.get(summaryUrl, this.bindStudentData, 'json');
+        alert("got to the end");
+    };
+
+    this.bindStudentData = function (data) {
+        ko.mapping.fromJS(data, this.studentSummary);
+        alert("mapped");
+    };
+};
+
+$('input').first().focus();
+ko.applyBindings(new SummaryViewModel());
