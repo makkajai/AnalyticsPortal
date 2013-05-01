69d649fc7cd9fa4aab42026a12e1d76ef60e184d
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2013-05-01 17:27:17 +0530

    Adding Images folder with logos in it
    This is to ensure that the report can show the corresponding
    activity logos.

--- src/AnalyticsPortal/AnalyticsPortal.ServiceModel/Types/Activity.cs
+++ src/AnalyticsPortal/AnalyticsPortal.ServiceModel/Types/Activity.cs
@@ -3,8 +3,10 @@
 namespace AnalyticsPortal.ServiceModel.Types
 {
     [Alias("analytics.activities")]
-    public class Activity 
+    public class Activity
     {
+        public const string LogoPath = "/Images/logos";
+
         [Alias("activity_id"), PrimaryKey]
         public int Id { get; set; }
 
@@ -13,6 +15,14 @@ namespace AnalyticsPortal.ServiceModel.Types
         public int Difficulty { get; set; }
 
         public string Logo { get; set; }
+
+        public string LogoUrl
+        {
+            get
+            {
+                return string.Format("{0}/{1}", LogoPath, Logo);
+            }
+        }
 
         public string Title { get; set; }
 
